5f7e1f52f414268db07b50483884408ca1bf54e0
Timon55 <forjob55@bk.ru>
2022-03-23 18:02:43 +0700

    Установил модуль django-selectmultifield, реализовал возможность создания вопросов одновременно заполняя ответы к ним через админку django.

--- edu_service/admin.py
+++ edu_service/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from edu_service.models import *
+
+class AnswersInLine(admin.TabularInline):
+    model = Answer
+    extra = 4
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswersInLine]
 
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Test)
+admin.site.register(Topic)
+admin.site.register(Answer)
--- edu_service/models.py
+++ edu_service/models.py
@@ -5,22 +5,58 @@ class Question(models.Model):
     text = models.CharField(verbose_name='Текст', max_length=250)
     num_right = models.IntegerField(verbose_name='Кол-во правильных ответов')
 
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+        ordering = ['id']
+
+
+    def __str__(self):
+        return self.text
+
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text=models.CharField(verbose_name='Ответ', max_length=250)
     right = models.BooleanField(verbose_name='Правильный', default=False)
 
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.text
+
 class Test(models.Model):
     title = models.CharField(verbose_name='Тема', max_length=250)
     Questions = models.ManyToManyField(Question)
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
 
 class Topic(models.Model):
     title = models.CharField(verbose_name='Название', max_length=250)
     theory = models.CharField(verbose_name='Теория', max_length=1000)
     tests = models.ManyToManyField(Test)
 
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+
 class Test_Result(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
     result = models.CharField(verbose_name='Результат', max_length=300)
+
+    def __str__(self):
+        return self.user.username
 
